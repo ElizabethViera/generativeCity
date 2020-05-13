f543c451e545a3dd1d26618cff4b1d2b0155c668
Elizabeth Viera <elizabeth.viera.codes@gmail.com>
2020-05-12 20:15:55 -0700

    add build to branch

---
+++ build/precache-manifest.5c5cd329ddda7326aff9cee9bc50f870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ca7239faa6aacd25652dc66ee046a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "166108fcd86e6c830d39",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cdc0a8a4e1526aabb827",
+    "url": "/static/js/2.d9460c57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9460c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166108fcd86e6c830d39",
+    "url": "/static/js/main.e62691f5.chunk.js"
+  },
+  {
+    "revision": "023d1060a2024caaeb53",
+    "url": "/static/js/runtime-main.24430e1e.js"
+  }
+]);

3a8691cbb9f52ce5bdd0cfedf00b8b36a59de04d
Elizabeth Viera <elizabeth.viera.codes@gmail.com>
2020-05-31 21:28:37 -0700

    create forest scene

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,206 @@ function randChoose<T>(options: Array<T>): T {
   const list = [...options];
   return list[Math.floor(Math.random() * list.length)];
 }
+function generateFallingPetals() {
+  const geometry = new THREE.Geometry()
+  const size = .1;
+  const index = geometry.vertices.length;
+  const vertices = [new THREE.Vector3(0, 0, 0)]
+    .flatMap(p => [new THREE.Vector3(size, 0, 0).add(p), new THREE.Vector3(-size, 0, 0).add(p)])
+    .flatMap(p => [new THREE.Vector3(0, size, 0).add(p), new THREE.Vector3(0, -size, 0).add(p)])
+    .flatMap(p => [new THREE.Vector3(0, 0, size).add(p), new THREE.Vector3(0, 0, -size).add(p)])
+
+  const white = [new THREE.Color("#FFF"), new THREE.Color("#FFF"), new THREE.Color("#FFF")]
+  const faces = [
+    // top face
+    new THREE.Face3(index + 4, index + 1, index + 5, undefined, white),
+    new THREE.Face3(index + 4, index, index + 1, undefined, white),
+
+
+    // right face
+    new THREE.Face3(index + 4, index + 2, index, undefined, white),
+    new THREE.Face3(index + 4, index + 6, index + 2, undefined, white),
+
+    // left face
+    new THREE.Face3(index + 4, index + 7, index + 6, undefined, white),
+    new THREE.Face3(index + 4, index + 5, index + 7, undefined, white),
+  ]
+
+  geometry.vertices.push(...vertices);
+  geometry.faces.push(...faces);
+  geometry.computeFaceNormals()
+  // return new THREE.Mesh(geometry, material)
+
+  const petalGroup = new THREE.Group()
+  const petalMeshes: THREE.Mesh[] = []
+  for (let x = -17; x <= -3; x++) {
+    for (let z = 6; z <= 22; z++) {
+      const randomTree = randBetween(0, 90);
+
+      // draw petals
+      if (randomTree > 12 && randomTree < 60) {
+        const treeTopHeight = randBetween(0, 8)
+        const pr = 255;
+        const pg = randBetween(150, 200);
+        const pb = randBetween(240, 250);
+        const pinkColor = "rgb(" + pr + ", " + pg + ", " + pb + ")"
+
+        const material = new THREE.MeshLambertMaterial({ color: pinkColor, vertexColors: true });
+        const mesh = new THREE.Mesh(geometry, material)
+        mesh.position.set(x, treeTopHeight, z);
+        petalGroup.add(mesh)
+        petalMeshes.push(mesh)
+      }
+
+    }
+  }
+
+  return {
+    group: petalGroup,
+    update: () => {
+      petalMeshes.forEach((m) => {
+        m.position.y -= .008
+        m.position.x = m.position.x + .008 * Math.cos(m.position.y)
+        if (m.position.y < 0) {
+          m.position.y = 8
+          m.position.x = randBetween(-17, -3)
+          m.position.z = randBetween(6, 22)
+        }
+      })
+    }
+  }
+
+
+}
+function generateForestScene() {
+  const geometry = new THREE.Geometry()
+  function addCube(pos: THREE.Vector3, { top = new THREE.Color("rgb(255,255,255)"), left = new THREE.Color("rgb(169, 169, 169)"), right = new THREE.Color("rgb(90,0,90)"), top2 }: Partial<Record<"top" | "left" | "right" | "top2", THREE.Color>>) {
+    const size = .5;
+    const index = geometry.vertices.length;
+    const vertices = [pos]
+      .flatMap(p => [new THREE.Vector3(size, 0, 0).add(p), new THREE.Vector3(-size, 0, 0).add(p)])
+      .flatMap(p => [new THREE.Vector3(0, size, 0).add(p), new THREE.Vector3(0, -size, 0).add(p)])
+      .flatMap(p => [new THREE.Vector3(0, 0, size).add(p), new THREE.Vector3(0, 0, -size).add(p)])
+
+
+
+
+
+    const faces = [
+      // top face
+      new THREE.Face3(index + 4, index + 1, index + 5, undefined, top),
+      new THREE.Face3(index + 4, index, index + 1, undefined, top2 || top),
+
+
+      // right face
+      new THREE.Face3(index + 4, index + 2, index, undefined, right),
+      new THREE.Face3(index + 4, index + 6, index + 2, undefined, right),
+
+      // left face
+      new THREE.Face3(index + 4, index + 7, index + 6, undefined, left),
+      new THREE.Face3(index + 4, index + 5, index + 7, undefined, left),
+    ]
+
+    geometry.vertices.push(...vertices);
+    geometry.faces.push(...faces);
+
+  }
+
+  function addRainCube(pos: THREE.Vector3, { top = new THREE.Color("rgb(255,255,255)"), left = new THREE.Color("rgb(169, 169, 169)"), right = new THREE.Color("rgb(90,0,90)"), top2 }: Partial<Record<"top" | "left" | "right" | "top2", THREE.Color>>) {
+    const size = .1;
+    const index = geometry.vertices.length;
+    const vertices = [pos]
+      .flatMap(p => [new THREE.Vector3(size, 0, 0).add(p), new THREE.Vector3(-size, 0, 0).add(p)])
+      .flatMap(p => [new THREE.Vector3(0, size, 0).add(p), new THREE.Vector3(0, -size, 0).add(p)])
+      .flatMap(p => [new THREE.Vector3(0, 0, size).add(p), new THREE.Vector3(0, 0, -size).add(p)])
+
+
+
+
+
+    const faces = [
+      // top face
+      new THREE.Face3(index + 4, index + 1, index + 5, undefined, top),
+      new THREE.Face3(index + 4, index, index + 1, undefined, top2 || top),
+
+
+      // right face
+      new THREE.Face3(index + 4, index + 2, index, undefined, right),
+      new THREE.Face3(index + 4, index + 6, index + 2, undefined, right),
+
+      // left face
+      new THREE.Face3(index + 4, index + 7, index + 6, undefined, left),
+      new THREE.Face3(index + 4, index + 5, index + 7, undefined, left),
+    ]
+
+    geometry.vertices.push(...vertices);
+    geometry.faces.push(...faces);
+
+  }
+  // ground
+  for (let x = -40; x <= 25; x++) {
+    for (let z = -25; z <= 45; z++) {
+      const genColor = (x: number, z: number) => {
+        const randomPink = randBetween(0, 80)
+        if (x > -18 && x < 1 && z > 2 && z < 22 && randomPink < 40) {
+          const rr = 245;
+          const rg = randBetween(120, 160);
+          const rb = randBetween(200, 230);
+          return "rgb(" + rr + ", " + rg + ", " + rb + ")"
+        } else {
+          const rr = randBetween(70, 100)
+          const rg = randBetween(200, 225)
+          const rb = randBetween(90, 115)
+          return "rgb(" + rr + ", " + rg + ", " + rb + ")"
+        }
+      }
+      const color = genColor(x, z)
+      addCube(new THREE.Vector3(x, 0, z), { top: new THREE.Color(color) });
+    }
+  }
+  // tree stumps
+  for (let x = -15; x <= -4; x++) {
+    for (let z = 7; z <= 20; z++) {
+      const randomTree = randBetween(0, 90);
+      if (randomTree < 12) {
+        // draw tree
+        const treeHeight = randBetween(5, 7)
+        for (let y = 0; y <= treeHeight; y++) {
+          const sr = randBetween(180, 215);
+          const sg = randBetween(180, 215);
+          const sb = randBetween(210, 215);
+          const sideColor = "rgb(" + sr + ", " + sg + ", " + sb + ")"
+          const tr = 255
+          const tg = randBetween(150, 200);
+          const tb = randBetween(240, 250);
+          const topColor = "rgb(" + tr + ", " + tg + ", " + tb + ")"
+          addCube(new THREE.Vector3(x, y, z), { top: new THREE.Color(topColor), left: new THREE.Color(sideColor), right: new THREE.Color(sideColor) });
+        }
+      }
+    }
+  }
+  for (let x = -17; x <= -3; x++) {
+    for (let z = 6; z <= 22; z++) {
+      const randomTree = randBetween(0, 90);
+
+      // draw treetops
+      if (randomTree > 12 && randomTree < 60) {
+        const treeTopHeight = randBetween(7, 8)
+        const pr = 255;
+        const pg = randBetween(150, 200);
+        const pb = randBetween(240, 250);
+        const pinkColor = "rgb(" + pr + ", " + pg + ", " + pb + ")"
+        addCube(new THREE.Vector3(x, treeTopHeight, z), { top: new THREE.Color(pinkColor), left: new THREE.Color(pinkColor), right: new THREE.Color(pinkColor) });
+      }
+    }
+
+  }
+
+  geometry.computeFaceNormals()
+  const material = new THREE.MeshLambertMaterial({ color: 0xffffff, vertexColors: true });
+  return new THREE.Mesh(geometry, material)
+
+}
 
 function generateScene() {
 
@@ -50,9 +250,6 @@ function generateScene() {
 
   }
 
-
-  // geometry.vertices = [new THREE.Vector3(-2, 3), new THREE.Vector3(2, -3), new THREE.Vector3(1, 2)]
-  // geometry.faces = [new THREE.Face3(0, 1, 2)]
 
   for (let x = -35; x <= 30; x++) {
     for (let z = -25; z <= 35; z++) {
@@ -125,8 +322,8 @@ async function draw(container: HTMLElement) {
   // var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   var camera = new THREE.OrthographicCamera(-10 * window.innerWidth / window.innerHeight, 10 * window.innerWidth / window.innerHeight, 10, -10, 0.1, 1000);
 
-  var light = new THREE.AmbientLight(new Color("#222"))
-  var dirLight = new THREE.DirectionalLight(new Color("#FFF"))
+  var light = new THREE.AmbientLight(new Color("#888"))
+  var dirLight = new THREE.DirectionalLight(new Color("#AAA"))
   dirLight.position.set(0.5, 1, 1)
   scene.add(light, dirLight)
   var renderer = new THREE.WebGLRenderer();
@@ -180,7 +377,7 @@ async function draw(container: HTMLElement) {
   doggo.traverse((obj) => {
     if (obj instanceof THREE.Mesh) {
       obj.geometry.computeVertexNormals();
-      obj.material = new THREE.MeshToonMaterial({ color: new THREE.Color(0xD2B48C) })
+      obj.material = new THREE.MeshToonMaterial({ color: new THREE.Color(0xADA09C) })
     }
   })
 
@@ -188,10 +385,11 @@ async function draw(container: HTMLElement) {
   doggo.scale.y = 0.3
   doggo.scale.z = 0.3
 
-  scene.add(ghostguy)
-  scene.add(doggo)
-  let city = generateScene()
-  scene.add(city)
+  // scene.add(ghostguy)
+  // scene.add(doggo)
+  let forest = generateForestScene()
+  let { group: petalGroup, update: updatePetals } = generateFallingPetals()
+  scene.add(forest, petalGroup)
 
 
   let t = 0
@@ -199,33 +397,34 @@ async function draw(container: HTMLElement) {
   let animationStop = false
 
   var animate = function () {
-
     requestAnimationFrame(animate);
-    doggo.position.y = 4
-    doggo.position.z = 7
-    if (animationStop == true) {
-      t += 0.05
-      ghostguy.position.y = 4 + 0.4 * (Math.cos(t))
-      ghostguy.setRotationFromEuler(new THREE.Euler(0, Math.PI - .65, 0))
-      ghostguy.rotateOnAxis(new THREE.Vector3(1, 0, 0), .4)
-      renderer.render(scene, camera);
-      return
-    }
-    t += 0.1
-    ghostguy.position.y = 4 + 0.4 * (Math.cos(t))
-    ghostguy.position.z = t
-    if (resets == 3 && t > 15) {
-      animationStop = true
-    }
-    ghostguy.setRotationFromEuler(new THREE.Euler(0, Math.PI, 0.04 * Math.cos(t * .8)))
-    doggo.setRotationFromEuler(new THREE.Euler(0, Math.PI, 0.04 * Math.cos(t * .8)))
-    if (t > 40) {
-      resets += 1
-      t = 0
-      scene.remove(city)
-      city = generateScene()
-      scene.add(city)
-    }
+    updatePetals()
+    // requestAnimationFrame(animate);
+    // doggo.position.y = 4
+    // doggo.position.z = 7
+    // if (animationStop == true) {
+    //   t += 0.05
+    //   ghostguy.position.y = 4 + 0.4 * (Math.cos(t))
+    //   ghostguy.setRotationFromEuler(new THREE.Euler(0, Math.PI - .65, 0))
+    //   ghostguy.rotateOnAxis(new THREE.Vector3(1, 0, 0), .4)
+    //   renderer.render(scene, camera);
+    //   return
+    // }
+    // t += 0.1
+    // ghostguy.position.y = 4 + 0.4 * (Math.cos(t))
+    // ghostguy.position.z = t
+    // if (resets == 3 && t > 15) {
+    //   animationStop = true
+    // }
+    // ghostguy.setRotationFromEuler(new THREE.Euler(0, Math.PI, 0.04 * Math.cos(t * .8)))
+    // doggo.setRotationFromEuler(new THREE.Euler(0, Math.PI, 0.04 * Math.cos(t * .8)))
+    // if (t > 40) {
+    //   resets += 1
+    //   t = 0
+    //   scene.remove(city)
+    //   city = generateScene()
+    //   scene.add(city)
+    // }
 
     renderer.render(scene, camera);
   };
